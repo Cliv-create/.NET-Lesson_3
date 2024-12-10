9f9d35d2514b5b04ba348a87b93d13f60c1e1fb5
Cliv-create <66003937+Cliv-create@users.noreply.github.com>
2024-12-10 17:50:45 +0200

    Add project files.

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Please, input steps amount: ");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"User input: {user_input}");
+            Console.Write("Please, input desired charachter to draw stairs: ");
+            char charachter = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"User input: {charachter}");
+
+            for (int i = 0; i < user_input; i++) // Number of steps
+            {
+                for (int x = 0; x < i; x++) // Draw spacing before drawing next step
+                {
+                    Console.Write("   ");
+                }
+
+                Console.WriteLine($"{charachter}"); // *
+
+                for (int x = 0; x < i; x++) // Draw spacing before drawing next step
+                {
+                    Console.Write("   ");
+                }
+
+                Console.WriteLine($"{charachter}{charachter}{charachter}"); // ***
+            }
+        }
+    }
+}
